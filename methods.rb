3e3644fe41b082ce4af723a5b197122e394525de
Marilyn Wren <Marilynwren64@yahoo.com>
2022-08-22 12:19:13 -0400

    commit tree f5492156ee3e97ed827ded9b6af50394ffe5f71f

--- methods.rb
+++ methods.rb
@@ -1,1 +1,120 @@
 # Your code here!
+# function greetProgrammer() {
+# console.log("Hello, programmer!");
+#}
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+greet_programmer
+
+#function greet(name) {
+# console.log(`Hello, ${name}!`);
+#}
+
+def greet(name = "Naureen")
+    puts "Hello, #{name}!"
+  end
+  
+greet
+
+#function greetWithDefault(name = "programmer") {
+#  console.log(`Hello, ${name}!`);
+#}
+
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+  end
+  
+  greet_with_default
+  
+  
+  greet_with_default"Naureen"
+  
+  
+
+
+
+
+
+def greet_With_Default(name = "Sunny")
+    puts "Hello, #{name}!"
+  end
+  
+  greet_With_Default
+
+
+ # function add(num1, num2) {
+ #  return num1 + num2;
+ #}
+
+ def add(num1, num2)
+    return num1 + num2
+  end
+
+  
+  sum = add(1, 2)
+
+
+  #You should be able to call this function with two arguments and get back its return value:
+  #const result = halve(4);
+  #console.log(result);
+ # => 2
+
+ def halve(number)
+    if number.class == Integer
+    return number/2 
+    else 
+    return nil
+end
+end
+   
+ 
+
+  
+  #result = halve(4)
+  #halve("two")
+
+ #You should be able to call this function with two arguments and get back its return value:
+ #const result = halve(4);
+ #console.log(result);
+ #=> 2
+
+ #def halve(number)
+ #   if number.class != String
+ #     return nil
+  #  end
+  
+   
+  #halve("two")
+  # => "ssoR boB"
+  #halve(4)
+  # => nil
+  
+
+
+
+
+ #If the function is called with an argument that isn't a number, it should return null:
+ #const result = halve("two")
+# => null
+#*/
+#function halve(number) {
+# if (typeof number !== "number") return null;
+
+ #return number / 2;
+#}
+
+
+  
+
+
+
+  
+ 
+  
+  
+  
+  
